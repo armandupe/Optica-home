583038d71a7f0aaaf219ed615babf308bc9ce854
Dmitrii Kudriavtsev <36885141+armandupe@users.noreply.github.com>
2019-03-20 11:41:32 +0300

    gulp ready

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,15 @@
 var gulp = require("gulp");
-
-var less = require("gulp-less"), // переводит less в CSS
+    less = require("gulp-less"), // переводит less в CSS
     cssnano = require("gulp-cssnano"), // Минимизация CSS
     autoprefixer = require('gulp-autoprefixer'), // Проставлет вендорные префиксы в CSS для поддержки старых браузеров
-    imagemin = require('gulp-imagemin'), // Сжатие изображение
     concat = require("gulp-concat"), // Объединение файлов - конкатенация
     uglify = require("gulp-uglify"), // Минимизация javascript
     rename = require("gulp-rename"); // Переименование файлов
 
-
-
-
-gulp.task("html", function() {
-    return gulp.src("src/*.html")
-    .pipe(gulp.dest("build"));
-});
-
+// gulp.task("html", function() {
+//     return gulp.src("src/*.html")
+//     .pipe(gulp.dest("build"));
+// });
 
 gulp.task("less", function() {
     return gulp.src(['src/less/styles.less', 'src/less/media.less'])
@@ -31,30 +25,16 @@ gulp.task("less", function() {
 });
 
 gulp.task("scripts", function() {
-    return gulp.src("src/js/*.js") // директория откуда брать исходники
-        .pipe(concat('scripts.js')) // объеденим все js-файлы в один 
-        .pipe(uglify()) // вызов плагина uglify - сжатие кода
-        .pipe(rename({ suffix: '.min' })) // вызов плагина rename - переименование файла с приставкой .min
-        .pipe(gulp.dest("build/js")); // директория продакшена, т.е. куда сложить готовый файл
-});
-
-gulp.task('imgs', function() {
-    return gulp.src("src/images/*.+(jpg|jpeg|png|gif)")
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{ removeViewBox: false }],
-            interlaced: true
-        }))
-        .pipe(gulp.dest("build/images"))
+    return gulp.src("src/js/*.js") 
+        .pipe(concat('scripts.js'))  
+        .pipe(uglify()) 
+        .pipe(rename({ suffix: '.min' })) 
+        .pipe(gulp.dest("build/js"));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('src/*.html', gulp.parallel('html'));
     gulp.watch('src/js/*.js', gulp.parallel('scripts'));
     gulp.watch('src/less/*.less', gulp.parallel('less'));
-    gulp.watch('src/images/*.+(jpg|jpeg|png|gif)', gulp.parallel('imgs'));
 });
 
-
-
-gulp.task('default', gulp.parallel('html', 'less', 'scripts', 'imgs', 'watch'));
+gulp.task('default', gulp.parallel('less', 'scripts', 'watch'));
